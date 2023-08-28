905d060086e3869b5c173b1bd190b23b069baa06
BokiCode <bojancodedev@gmail.com>
2023-08-28 14:15:39 +0200

    added script folder

---
+++ script/hamburger_menu.js
@@ -0,0 +1,6 @@
+var hamburgerBtn = document.getElementById("hamburger-menu-btn");
+var navbarUl = document.querySelector("navbar ul");
+
+hamburgerBtn.addEventListener("click", function () {
+  navbarUl.style.display = "block";
+});
