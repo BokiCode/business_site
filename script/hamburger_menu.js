c6dbf975fd0d1e41ab1c88175a82f5dd9874c566
BokiCode <bojancodedev@gmail.com>
2023-08-28 14:56:55 +0200

    added responsivnes to a hero section

--- script/hamburger_menu.js
+++ script/hamburger_menu.js
@@ -1,6 +1,1 @@
-var hamburgerBtn = document.getElementById("hamburger-menu-btn");
-var navbarUl = document.querySelector("navbar ul");
 
-hamburgerBtn.addEventListener("click", function () {
-  navbarUl.style.display = "block";
-});
